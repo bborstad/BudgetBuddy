edc2d83ba4e295c91bc816e55dcb7053c9e9c2c1
Logan Newman <loganewman10@gmail.com>
2020-10-01 16:08:39 -0500

    Generated Scaffold for Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_165821) do
+ActiveRecord::Schema.define(version: 2020_10_01_210204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.decimal "balance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
