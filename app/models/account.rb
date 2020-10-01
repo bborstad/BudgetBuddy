ff1b01c8330e4e98777411912f1642005ece429f
Logan Newman <loganewman10@gmail.com>
2020-10-01 16:28:39 -0500

    Created Association between Accounts and Users via migration

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,25 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_accounts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Account < ApplicationRecord
+
+    belongs_to(
+        :user,
+        class_name: 'User',
+        foreign_key: 'user_id',
+        inverse_of: :accounts
+    )
+
+
+
 end
---
+++ db/migrate/20201001212538_add_user_fk_col_to_accounts.rb
@@ -0,0 +1,5 @@
+class AddUserFkColToAccounts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :accounts, :user, foreign_key: true
+  end
+end
