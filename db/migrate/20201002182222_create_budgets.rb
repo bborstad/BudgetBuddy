21df5ce9cba0f187c14ff6134d1391aeee545414
Benjamin Borstad <bborstad@gmail.com>
2020-10-11 09:37:20 -0500

    WIP

--- db/migrate/20201002182222_create_budgets.rb
+++ db/migrate/20201002182222_create_budgets.rb
@@ -1,8 +1,8 @@
 class CreateBudgets < ActiveRecord::Migration[6.0]
   def change
     create_table :budgets do |t|
-      t.date "date", default: -> { 'CURRENT_DATE' }
-
+      t.date :date, default: -> { 'CURRENT_DATE' }
+      t.month :month, 
       t.timestamps
     end
   end
