308750bc5d6cb6aeadcab8f8033cd893e3d55794
Isaiah Bryant <icbryant9@gmail.com>
2020-10-13 17:22:57 -0500

    Working on goals now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show 
     @user = User.find(params[:id])
-    @goals = Goal.all
+    @goals = Goal.where(user_id:@user.id).order(created_at: :desc)
     @posts = Post.where(user_id:@user.id).order(created_at: :desc)
 
   end 
