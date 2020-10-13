2c625780cef6904a36c01b3efeea2f3a5d467164
Isaiah Bryant <icbryant9@gmail.com>
2020-10-13 16:43:12 -0500

    User profile post complete.  Still have to add comments functinality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def show 
     @user = User.find(params[:id])
     @goals = Goal.all
-    @posts = Post.where(user_id:@user.id)
+    @posts = Post.where(user_id:@user.id).order(created_at: :desc)
 
   end 
 
