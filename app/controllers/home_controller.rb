d5863e038983a47d6127d4fff2e7d6cb455f8b4d
Isaiah Bryant <icbryant9@gmail.com>
2020-10-14 17:04:52 -0500

    Stoping point

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @user = current_user
+    @goals = Goal.where(user_id:@user.id).order(ppercent: :asc)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def terms
