7daac8a3d1b710ce7532630bfae3f080616fa05c
Isaiah Bryant <icbryant9@gmail.com>
2020-10-14 20:28:23 -0500

    Completed side bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
     @user = current_user
     @goals = Goal.where(user_id:@user.id).order(ppercent: :asc)
