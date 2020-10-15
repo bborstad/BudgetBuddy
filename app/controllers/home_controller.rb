5f5c78607c161816b2e34154cbb8f85dfd14771c
Isaiah Bryant <icbryant9@gmail.com>
2020-10-14 23:42:36 -0500

    Finished home page post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @post = Post.new
     @user = current_user
     @goals = Goal.where(user_id:@user.id).order(ppercent: :asc)
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
   end
 
   def terms
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_back fallback_location: root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
