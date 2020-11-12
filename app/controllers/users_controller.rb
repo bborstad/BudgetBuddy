49255fe106c55093e5a653ae7d24b149f2d82e43
Benjamin Borstad <bborstad@gmail.com>
2020-11-12 09:43:45 -0600

    WIP: search working, just need to center it in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
+  def search
+    @users = User.where("first_name || ' ' || last_name ILIKE ?", "%"+params[:q]+"%")
+  end
 
 
   def show 
     @user = User.find(params[:id])
     @goals = Goal.where(user_id:@user.id).order(ppercent: :asc)
     @posts = Post.where(user_id:@user.id).order(created_at: :desc)
-
   end 
 
 
