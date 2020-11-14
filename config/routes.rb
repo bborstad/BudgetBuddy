2a493aef879c2b330e6fb633b3134bc5f8cfce17
Benjamin Borstad <bborstad@gmail.com>
2020-11-13 19:14:34 -0600

    Initial commit with scaffold set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :transactions
   resources :user_options
   get 'search', to: 'users#search'
   
---
+++ db/migrate/20201114011417_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.float :amount
+      t.date :date
+      t.string :source
+
+      t.timestamps
+    end
+  end
+end
