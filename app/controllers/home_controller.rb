f7ae60ec9c2e869265bdfdee7b4b6219450d5022
Isaiah Bryant <icbryant9@gmail.com>
2020-11-17 16:30:21 -0600

    shared postcard created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @post = Post.new
+    @post = Post.new #new post
     @user = current_user
-    @goals = Goal.where(user_id:@user.id).order(ppercent: :asc)
+    @goals = Goal.where(user_id:@user.id).order(ppercent: :asc) #Get users goals
 
-    @userlist = Follow.where(:requestor => @current_user.id).pluck(:following)
+    @userlist = Follow.where(:requestor => @current_user.id).pluck(:following) # get list of follows
     @userlist.push( @current_user.id)
-    @posts = Post.where(user_id:@userlist).order(created_at: :desc)
+    @posts = Post.where(user_id:@userlist).order(created_at: :desc) #filters posts with users you follow
     
     @goalss = Goal.where(user_id:@user.id).order(ppercent: :asc)
     @comments = Comment.where(post_id: @post).order("created_at DESC")
